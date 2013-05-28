ca0009c143f3515db9810c1c9a3deff0416a41f7
Joe Niemi <niemijoe@hotmail.com>
2013-05-28 13:54:17 +0300

    uusYritys

---
+++ uusYritys/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '19d128660564c1d269a183ff2c936d82deda72c2f51b2e6737b9329ea8fcb019e48f68f840f084d0474e88a65b3b8c40b8f6ad5fe31e5115fe113a15755130ae'
